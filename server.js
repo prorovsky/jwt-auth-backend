a3d4369e8fd7b83dc3380f8458765e2f25277f57
Pavel Prorovsky <prorovsky@gmail.com>
2017-12-11 20:14:36 +0300

    add body-parser and routes

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+const posts = [
+    {message: 'hello'},
+    {message: 'hi'}
+];
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.listen(3000);
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/register', (req, res) => {
+    const userData = req.body;
+    console.log(userData.email);
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+    console.log('app running and listen localhost port 3000');
+});
